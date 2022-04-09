68e7b3301bfb9bce2358b8cbce9510a255a80d28
SvetlanaM <svetlana@kompilator.cz>
2022-04-09 23:28:46 +0200

    Update

--- js/navbar.js
+++ js/navbar.js
@@ -1,31 +1,33 @@
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 100) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+$(window).scroll(function () {
+	if ($('.navbar').offset().top > 100) {
+		$('.navbar-fixed-top').addClass('top-nav-collapse');
+	} else {
+		$('.navbar-fixed-top').removeClass('top-nav-collapse');
+	}
 });
 
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 100) {
-        $(".test").removeClass('btn-remove')
-    } else {
-        $(".test").addClass("btn-remove");
-    }
+$(window).scroll(function () {
+	if ($('.navbar').offset().top > 100) {
+		$('.test').removeClass('btn-remove');
+	} else {
+		$('.test').addClass('btn-remove');
+	}
 });
-
-
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(function () {
+	$('.page-scroll a').bind('click', function (event) {
+		var $anchor = $(this);
+		$('html, body')
+			.stop()
+			.animate(
+				{
+					scrollTop: $($anchor.attr('href')).offset().top,
+				},
+				1500,
+				'easeInOutExpo'
+			);
+		event.preventDefault();
+	});
 });
-
-
